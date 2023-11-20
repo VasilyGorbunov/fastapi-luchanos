eb3dd38bb89ee768462857666326d9ecee3405c0
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-11-20 23:10:31 +0300

    1 - 31:40

--- main.py
+++ main.py
@@ -29,10 +29,11 @@ class User(Base):
 
 class UserDAL:
     """Data Access Layer for operating user info"""
+
     def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
 
-    async def create_user(self, name:str, surname:str, email:str)->User:
+    async def create_user(self, name: str, surname: str, email: str) -> User:
         new_user = User(
             name=name,
             surname=surname,
@@ -42,4 +43,3 @@ class UserDAL:
         self.db_session.add(new_user)
         await self.db_session.flush()
         return new_user
-
