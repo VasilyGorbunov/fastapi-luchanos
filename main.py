945636931977bd58a436f0de12903554eb695353
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-11-21 16:42:53 +0300

    2 - 18:20

--- main.py
+++ main.py
@@ -1,45 +1,13 @@
 from fastapi import FastAPI
-import uvicorn
 from fastapi.routing import APIRouter
-from sqlalchemy import Column, Boolean, String
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import declarative_base
-import settings
-from sqlalchemy.dialects.postgresql import UUID
-import uuid
-import re
-from fastapi import HTTPException
-from pydantic import BaseModel, EmailStr, field_validator
 
-engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession, )
+from api.handlers import user_router
 
-Base = declarative_base()
+app = FastAPI(
+    title="Luchanos FastAPI"
+)
 
+main_api_router = APIRouter()
 
-class User(Base):
-    __tablename__ = 'users'
-
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = Column(String, nullable=False)
-    surname = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    is_active = Column(Boolean, default=True)
-
-
-class UserDAL:
-    """Data Access Layer for operating user info"""
-
-    def __init__(self, db_session: AsyncSession):
-        self.db_session = db_session
-
-    async def create_user(self, name: str, surname: str, email: str) -> User:
-        new_user = User(
-            name=name,
-            surname=surname,
-            email=email,
-        )
-
-        self.db_session.add(new_user)
-        await self.db_session.flush()
-        return new_user
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(main_api_router)
